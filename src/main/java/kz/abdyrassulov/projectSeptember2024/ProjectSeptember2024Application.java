7b4e4c6284d23b11a05ea995a7c0ce03c7749eff
Abdyrassulov_Dinmukhammet <abd944@mail.ru>
2024-09-08 13:55:17 +0500

    Second commit

---
+++ src/main/java/kz/abdyrassulov/projectSeptember2024/ProjectSeptember2024Application.java
@@ -0,0 +1,13 @@
+package kz.abdyrassulov.projectSeptember2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSeptember2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSeptember2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/kz/abdyrassulov/projectSeptember2024/ProjectSeptember2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.abdyrassulov.projectSeptember2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSeptember2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
